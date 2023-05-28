2856f650f2a75f042ab3daeede3a6fbf8293b745
Adedamola Adeyemi <Adedamolaiadeyemi@gmail.com>
2023-05-28 13:27:41 +0100

    Update AdminApplicationTests.java

--- src/test/java/com/modeltech/instantissuance/AdminApplicationTests.java
+++ src/test/java/com/modeltech/instantissuance/AdminApplicationTests.java
@@ -2,7 +2,7 @@ package com.modeltech.instantissuance;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit4.SpringRunner; 
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
